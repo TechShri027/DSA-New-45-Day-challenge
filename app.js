57f6323290b6c0717e28d0036cb68c518ee7fb63
TechShri027 <shrishtibansal2705@gmail.com>
2026-02-09 08:34:52 +0530

    dsa array1 question

---
+++ app.js
@@ -0,0 +1,10 @@
+// 1. Reverse an array.
+// Output: [5, 4, 3, 2, 1]
+
+//  const arr = [1, 2, 3, 4, 5];
+
+// let reverse=[]
+// for(let i=arr.length-1; i>=0; i--){
+//    reverse.push(arr[i])
+// }
+// console.log(reverse)
