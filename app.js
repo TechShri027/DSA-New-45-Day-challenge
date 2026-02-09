40d016a664b1a54da50d01164823ed248418e043
TechShri027 <shrishtibansal2705@gmail.com>
2026-02-09 09:08:03 +0530

    new code added

--- app.js
+++ app.js
@@ -8,3 +8,42 @@
 //    reverse.push(arr[i])
 // }
 // console.log(reverse)
+
+
+// ----------------------------------------------------------------------------------------------------------------
+
+
+// 1. Reverse an array.
+// Output: [5, 4, 3, 2, 1]    but in same array
+
+// const arr=[1,2,3,4,5]
+// for(let i=0; i<arr.length/2; i++){
+//     let temp=arr[i]
+//     arr[i]=arr[arr.length-1-i]
+//     arr[arr.length-1-i]=temp
+// }
+// console.log(arr)
+
+
+// ----------------------------------------------------------------------------------------------------------------
+
+// Flatten a nested array.
+// Output: [1, 2, 3, 4, 5]
+
+
+const arr = [1, [2, 3], [4, [5]]];
+const newarr=[]
+const flattern=(arr)=>{
+arr.map(element => {
+    if(Array.isArray(element)){
+        flattern(element)
+    }else{
+        newarr.push(element)
+    }
+
+})
+}
+flattern(arr)
+console.log(newarr)
+
+
